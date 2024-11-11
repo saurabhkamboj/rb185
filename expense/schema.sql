ba6b0929951dff0495e9f50bffa57ef005f57519
Saurabh Kamboj <skamboj97@gmail.com>
2024-11-11 17:59:17 +0530

    Modify file

--- expense/schema.sql
+++ expense/schema.sql
@@ -2,12 +2,12 @@ CREATE TABLE expenses (
   id serial PRIMARY KEY,
   amount numeric(6, 2) NOT NULL,
   memo text NOT NULL,
-  created_on date DEFAULT CURRENT_DATE
+  created_on date NOT NULL
 );
 
-ALTER TABLE expenses ADD CHECK (amount >= 0.00);
+ALTER TABLE expenses ADD CHECK (amount >= 0.01);
 
-INSERT INTO expenses (amount, memo)
-  VALUES (13.99, 'Coffee'),
-    (3.00, 'Pencil'),
-    (9.79, 'Github Copilot');
+INSERT INTO expenses (amount, memo, created_on)
+  VALUES (13.99, 'Coffee', CURRENT_DATE),
+    (3.00, 'Pencil', CURRENT_DATE),
+    (9.79, 'Github Copilot', CURRENT_DATE);
